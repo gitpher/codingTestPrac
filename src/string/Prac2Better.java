3b6914f532d85066db79607ec9d613f699f6d6e9
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-09-07 02:38:53 +0900

    <String> 2. 대소문자 변환 (개선)

---
+++ src/string/Prac2Better.java
@@ -0,0 +1,22 @@
+package string;
+
+import java.nio.charset.Charset;
+
+public class Prac2Better {
+
+    public static void main(String[] args) {
+
+        String str = "StuDy";
+
+        String result = "";
+
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) result += Character.toLowerCase(c);
+            else result += Character.toUpperCase(c);
+        }
+
+        System.out.println(result);
+    }
+
+
+}
