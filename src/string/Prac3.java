e3f4cbe4a164fd730f19fc1251e90d553b80a8ef
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-09-08 16:53:00 +0900

    <String> 3. 문장 속 단어(indexOf(), substring())

---
+++ src/string/Prac3.java
@@ -0,0 +1,30 @@
+package string;
+
+import java.util.List;
+
+public class Prac3 {
+
+    public static void main(String[] args) {
+
+
+
+        String str = "it is time to study";
+
+
+        List<String> strs = List.of(str.split(" "));
+
+        int max = 0;
+        String result = "";
+
+        for (String s : strs) {
+            if (s.length() > max) {
+                result = s;
+                max = s.length();
+            }
+
+        }
+
+        System.out.println(result);
+
+    }
+}
