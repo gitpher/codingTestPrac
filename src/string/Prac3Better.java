bfccb90a389d04a27cf0718d4e7bcd2550411c09
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-09-15 00:44:39 +0900

    <String> 3. 문장 속 단어(indexOf(), substring()) (개선)

---
+++ src/string/Prac3Better.java
@@ -0,0 +1,22 @@
+//package string;
+//
+//public class Prac3Better {
+//    public static void main(String[] args) {
+//
+//        String str = "it is time to study";
+//        String answer = "";
+//
+//        int m = Integer.MIN_VALUE;
+//        String[] strings = str.split(" ");
+//
+//        for (String s : strings) {
+//            if (s.length() > m) {
+//                m = s.length();
+//                answer = s;
+//            }
+//        }
+//
+//        System.out.println(answer);
+//
+//    }
+//}
