1dde62eaa7023f70954d98de873d4062c79a90c9
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-09-15 20:28:29 +0900

    <String> 7. 회문문자열 (개선)

--- src/string/Prac7Better.java
+++ src/string/Prac7Better.java
@@ -1,4 +1,19 @@
-package string;
-
-public class Prac7Better {
-}
+//package string;
+//
+//public class Prac7Better {
+//    public static void main(String[] args) {
+//
+//        String in = "yooy";
+//        in = in.toLowerCase();
+//        String out = "YES";
+//
+//        int len = in.length();
+//
+//        for (int i=0; i<len/2; i++){
+//            if(in.charAt(i) != in.charAt(len-i-1)) out = "NO"; break;
+//        }
+//
+//        System.out.println(out);
+//
+//    }
+//}
