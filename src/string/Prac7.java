336a6052256b3512492c25effa7f02ddffb0037c
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-09-15 16:50:27 +0900

    <String> 7. 회문문자열

---
+++ src/string/Prac7.java
@@ -0,0 +1,16 @@
+//package string;
+//
+//public class Prac7 {
+//    public static void main(String[] args) {
+//
+//
+//        String in = "goog";
+//        String out = "";
+//        String rs = new StringBuilder(in).reverse().toString();
+//
+//        out = (rs.toLowerCase().equals(in.toLowerCase())) ? "YES" : "NO";
+//
+//        System.out.println(out);
+//
+//    }
+//}
