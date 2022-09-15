b8cb0786d1f6f36434ae11c1716d6ef2d3be1732
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-09-15 16:33:10 +0900

    <String> 6. 중복문자제거 (개선)

---
+++ src/string/Prac6Better.java
@@ -0,0 +1,16 @@
+//package string;
+//
+//public class Prac6Better {
+//    public static void main(String[] args) {
+//
+//        String in = "ksetkkset";
+//        String out = "";
+//
+//        for (int i = 0; i < in.length(); i++) {
+//            if (in.indexOf(in.charAt(i)) == i) out += in.charAt(i);
+//        }
+//
+//        System.out.println(out);
+//
+//    }
+//}
