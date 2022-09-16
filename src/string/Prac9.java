5def312627c488186321c83a1f816cfaa2509f33
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-09-17 01:59:50 +0900

    <String> 9. 숫자만 추출 (개선)

--- src/string/Prac9.java
+++ src/string/Prac9.java
@@ -1,22 +1,22 @@
-package string;
-
-public class Prac9 {
-    public static void main(String[] args) {
-
-        String in = "tge0a1h205er";
-        int out = 0;
-
-        for (char c : in.toCharArray()) {
-            if (c >= 48 && c <= 57) {
-                out = out * 10 + (c-48);
-            }
-        }
-
-        System.out.println(out);
-
-
-
-
-
-    }
-}
+//package string;
+//
+//public class Prac9 {
+//    public static void main(String[] args) {
+//
+//        String in = "tge0a1h205er";
+//        int out = 0;
+//
+//        for (char c : in.toCharArray()) {
+//            if (c >= 48 && c <= 57) {
+//                out = out * 10 + (c-48);
+//            }
+//        }
+//
+//        System.out.println(out);
+//
+//
+//
+//
+//
+//    }
+//}
---
+++ src/string/Prac9Better.java
@@ -0,0 +1,19 @@
+package string;
+
+public class Prac9Better {
+    public static void main(String[] args) {
+
+        String in = "aslfkaj124314l1k13j4";
+        String out = "";
+
+        for (char c : in.toCharArray()) {
+            if (Character.isDigit(c)) {
+                out += c;
+            }
+        }
+
+        System.out.println(Integer.parseInt(out));
+
+
+    }
+}
