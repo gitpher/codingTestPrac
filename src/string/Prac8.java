f0b0be79631d7b7dcd0ce3425e2ce1734d9b7e99
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-09-17 01:51:38 +0900

    <String> 9. 숫자만 추출

--- src/string/Prac8.java
+++ src/string/Prac8.java
@@ -1,19 +1,19 @@
-package string;
-
-public class Prac8 {
-    public static void main(String[] args) {
-
-        String in = "found7, time: study; Yduts; emit, 7Dnuof";
-        String out = "NO";
-
-        in = in.toUpperCase().replaceAll("[^A-Z]", "");
-        String tmp = new StringBuilder(in).reverse().toString();
-        if (tmp.equals(in)) {
-            out = "YES";
-        }
-
-        System.out.println(out);
-
-
-    }
-}
+//package string;
+//
+//public class Prac8 {
+//    public static void main(String[] args) {
+//
+//        String in = "found7, time: study; Yduts; emit, 7Dnuof";
+//        String out = "NO";
+//
+//        in = in.toUpperCase().replaceAll("[^A-Z]", "");
+//        String tmp = new StringBuilder(in).reverse().toString();
+//        if (tmp.equals(in)) {
+//            out = "YES";
+//        }
+//
+//        System.out.println(out);
+//
+//
+//    }
+//}
