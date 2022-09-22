674cec0e3be70598e05ef0adf5696a4452e7c3c2
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-09-22 16:00:32 +0900

    <String> 11. 문자열 압축

--- src/string/Prac10.java
+++ src/string/Prac10.java
@@ -1,47 +1,47 @@
-package string;
-
-import java.util.Arrays;
-
-public class Prac10 {
-
-
-    public static void main(String[] args) {
-
-
-        String str = "teachermode";
-        char in = 'e';
-
-        // 거리 숫자 배열
-        int[] ints = new int[str.length()];
-        int p = 1000;
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == in) {
-                p=0;
-                ints[i] = p;
-            } else {
-                p++;
-                ints[i] = p;
-            }
-        }
-        p = 1000;
-        for (int i = str.length() - 1; i >= 0; i--) {
-            if (str.charAt(i) == in) {
-                p=0;
-            } else {
-                p++;
-                ints[i] = Math.min(ints[i], p);
-            }
-        }
-
-        for (int i : ints) {
-            System.out.print(i + "");
-        }
-
-    }
-
-
-
-
-
-}
+//package string;
+//
+//import java.util.Arrays;
+//
+//public class Prac10 {
+//
+//
+//    public static void main(String[] args) {
+//
+//
+//        String str = "teachermode";
+//        char in = 'e';
+//
+//        // 거리 숫자 배열
+//        int[] ints = new int[str.length()];
+//        int p = 1000;
+//
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == in) {
+//                p=0;
+//                ints[i] = p;
+//            } else {
+//                p++;
+//                ints[i] = p;
+//            }
+//        }
+//        p = 1000;
+//        for (int i = str.length() - 1; i >= 0; i--) {
+//            if (str.charAt(i) == in) {
+//                p=0;
+//            } else {
+//                p++;
+//                ints[i] = Math.min(ints[i], p);
+//            }
+//        }
+//
+//        for (int i : ints) {
+//            System.out.print(i + "");
+//        }
+//
+//    }
+//
+//
+//
+//
+//
+//}
